53f790f3283868a1d6f16e0bfa9742365f1a524d
Tobit Raff <0xtobit@gmail.com>
2012-11-17 05:57:54 -0500

    Added .gitingore to install and rm scripts.

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -s $cwd/.bashrc.tobit .bashrc.tobit
 ln -s $cwd/.vimrc .vimrc
 ln -s $cwd/.tmux.conf .tmux.conf
 ln -s $cwd/XTerm XTerm
+ln -s $cwd/.gitignore .gitignore
 cd $HOME/.config/ipython/profile_default/
 ln -s $cwd/ipython_config.py ipython_config.py
 cd $cwd
--- rm.sh
+++ rm.sh
@@ -5,4 +5,5 @@ rm -f $HOME/.bashrc.tobit
 rm -f $HOME/.vimrc
 rm -f $HOME/.tmux.conf
 rm -f $HOME/XTerm
+rm -f $HOME/.gitignore
 rm -f $HOME/.config/ipython/profile_default/ipython_config.py
